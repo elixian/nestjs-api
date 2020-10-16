a0493d66b205e922f057fab439a7f8e8400b80ac
Franck Colombo <franck.colombo@cnav.fr>
2020-10-16 18:43:31 +0200

    fix bcrypt load module

--- src/shared/hash/hash.security.ts
+++ src/shared/hash/hash.security.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import {  genSalt,hash ,compare} from 'bcrypt';
+import { compare, genSalt, hash } from 'bcrypt';
+
 
 @Injectable()
 export class HashSecurity{
