fca2f5d7ac93230b9ee0a4c535db3828c15d2625
Franck Colombo <franck.colombo@cnav.fr>
2020-10-21 19:24:21 +0200

    change vue 2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,48 +1,6 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <button @click="postUser">Send request</button>
-    <code>
-      {{ info }}
-    </code>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Welcome to Your Vue.js App</h1>
   </div>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
-import axios from "axios";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-  data() {
-    return {
-      info: null,
-    };
-  },
-  methods: {
-    postUser() {
-      axios
-        .post("http://localhost:3000/auth/signup", {
-          username: "francky",
-          password: "12345",
-        })
-        .then((response) => (this.info = response));
-    },
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
